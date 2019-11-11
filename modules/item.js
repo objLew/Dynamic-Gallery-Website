20b011bf559ed37ffa980afd947bc340b82981a1
Lewis Lovette <lovettel@coventry.ac.uk>
2019-11-11 13:04:55 +0000

    set up for feature branch

--- modules/item.js
+++ modules/item.js
@@ -1,23 +1,21 @@
 
 'use strict'
 
-const bcrypt = require('bcrypt-promise')
 // const fs = require('fs-extra')
 const mime = require('mime-types')
 const sqlite = require('sqlite-async')
-const saltRounds = 10
 
-module.exports = class User {
+module.exports = class items {
 
     constructor(dbName = ':memory:') {
 		return (async() => {
 			this.db = await sqlite.open(dbName)
-			// we need this table to store the user accounts
-            const sql = 'CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, smallPic BLOB, picture BLOB, name TEXT, price INTEGER, shortDesc TEXT, longDesc TEXT, sold BOOLEAN);'
+			// creating a table to store item information
+            const sql = 'CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY AUTOINCREMENT, smallPic BLOB, picture BLOB, name TEXT, price INTEGER, shortDesc TEXT, longDesc TEXT, sold BOOLEAN);'
             
 			await this.db.run(sql)
 			return this
 		})()
 	}
-
+	
 }
